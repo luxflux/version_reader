04af976856f997217405dff39a3f9709f37b69f1
Raffael Schmid <raffael@yux.ch>
2012-07-14 16:50:16 +0200

    fix rails loading

--- lib/version_reader/railtie.rb
+++ lib/version_reader/railtie.rb
@@ -1,8 +1,14 @@
 class VersionReader
   class Railtie < Rails::Railtie
 
-    def version
-      @version ||= VersionReader.new(File.join(Rails.root, 'VERSION'))
+    initializer "version_reader.add_app_version_variable" do |app|
+      app.class.class_eval do
+        attr_accessor :version
+      end unless app.respond_to?(:version)
+    end
+
+    initializer "version_reader.set_app_version" do |app|
+      app.version = VersionReader.new(File.join(::Rails.root, 'VERSION'))
     end
 
   end
